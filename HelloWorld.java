69683d9b8f2d058c806d43d929232193610aabaa
KephM <kephryn.merkley@tooeleschools.org>
2019-08-28 09:37:57 -0600

    pushing HelloWorld to git

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/* 
+Kephryn Merkley
+8-28-2019
+Mobile App Development
+HelloWorld assignment
+*/
+
+class HelloWorld
+     {
+	//this is a simple Hello World assignment
+
+	public static void main(String[] args)	
+     
+	{
+		//program statement
+		System.out.println("Hello, World");
+	}
+     }
