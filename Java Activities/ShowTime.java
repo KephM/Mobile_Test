242eb7ed6fd2a108c9b5faa2e9d267091a636e30
KephM <kephryn.merkley@tooeleschools.org>
2019-08-30 09:19:48 -0600

    push to git

---
+++ Java Activities/ShowTime.java
@@ -0,0 +1,15 @@
+/*
+Kephryn Merkley
+8-30-19
+ShowTime project
+*/
+
+import java.util.Calendar;
+
+public class ShowTime 
+{
+   public static void main(String[] args) 
+   {
+	System.out.println(Calendar.getInstance().getTime());
+   }
+}
